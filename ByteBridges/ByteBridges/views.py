7222af7fa67c5d1ef731ab484e8397485c90ad57
Occulia <estgv18986@alunos.estgv.ipv.pt>
2023-11-22 11:51:05 +0000

    update

--- ByteBridges/ByteBridges/views.py
+++ ByteBridges/ByteBridges/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
 
+def Login(request):
+    return render(request, "Login.html") 
 
 def IndexPage(request):
-    return render(request, template_name='base_template.html')  
+    return render(request, template_name='base_template.html') 
+
+
+def Clients(request):
+    return render(request, template_name='dashboard.html')   
 
 
 
