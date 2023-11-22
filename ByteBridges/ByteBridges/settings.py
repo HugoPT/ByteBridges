82ab77d835c562c516da30bd668dbc74a38fb10d
hugosantos <hugo.santos@i9box.net>
2023-11-22 09:26:11 +0000

    BD connection

--- ByteBridges/ByteBridges/settings.py
+++ ByteBridges/ByteBridges/settings.py
@@ -74,10 +74,16 @@ WSGI_APPLICATION = 'ByteBridges.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ByteBridges',
+        'USER': 'master',
+        'PASSWORD': 'safeP@ss0rd4ever',
+        'HOST': 'apps.i9soft.pt',
+        'PORT': '5455',
     }
+
 }
 
 
