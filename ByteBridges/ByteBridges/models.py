182a3b3ffb12c6cf9a1fdd1371e6820877c12338
hugosantos <hugo.santos@i9box.net>
2023-12-01 20:49:35 +0000

    added list suppliers added export buttons on datatables added i8t Portuguese to datatables links to email and phone

--- ByteBridges/ByteBridges/models.py
+++ ByteBridges/ByteBridges/models.py
@@ -5,11 +5,11 @@ class Supplier(models.Model):
     idsupplier = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     nif = models.TextField()
-    email = models.CharField(max_length=100, unique=True)
     address = models.TextField()
     zipcode = models.CharField(max_length=100, unique=True)
     city = models.TextField()
     phone = models.TextField()
+    email = models.CharField(max_length=100, unique=True)
     obs = models.TextField()
 
     def __str__(self):
--- ByteBridges/ByteBridges/views.py
+++ ByteBridges/ByteBridges/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Supplier
-from django.db import connection
+from django.db import connections
 
 
 def Login(request):
@@ -21,6 +21,7 @@ def IndexPage(request):
 
 # Clients
 def clientsList(request):
+    # return redirect('dashboard')
     return render(request, template_name='clientsList.html')
 
 
@@ -33,14 +34,15 @@ def clientsCreate(request):
         city = request.POST.get('city')
         phone = request.POST.get('phone')
         obs = request.POST.get('obs')
-        with connection['admin'].cursor() as cursor:
+        with connections['admin'].cursor() as cursor:
             # Call the stored procedure using the CALL statement
             cursor.execute("CALL sp_suppliers_create(%s,%s,%s,%s,%s,%s,%s,%s,)",
                            [name, nif, address, zipcode, city, phone, obs])
             # If the stored procedure returns results, you can fetch them
             result = cursor.fetchall()
             print(result)
-        print(f"Inserted client " + name + nif + address + zipcode + city + phone + obs)
+        print(
+            f"Inserted client " + name + " " + nif + " " + address + " " + zipcode + " " + city + " " + phone + " " + obs)
         # return render(request, 'your_template.html', {'result': result})
         return redirect('dashboard')
     # return the form
@@ -49,32 +51,35 @@ def clientsCreate(request):
 
 # Suppliers
 def suppliersList(request):
-    return render(request, template_name='suppliersList.html')
+    with connections['admin'].cursor() as cursor:
+        # Call the stored procedure using the CALL statement
+        cursor.execute("select  * from view_suppliers_list", [])
+        # If the stored procedure returns results, you can fetch them
+        result = cursor.fetchall()
+        print(result)
+        suppliers = [Supplier(*row) for row in result]
+        return render(request,'suppliersList.html',{'suppliers': suppliers})
 
 
 def suppliersCreate(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         nif = request.POST.get('nif')
-        email = request.POST.get('email')
         address = request.POST.get('address')
         zipcode = request.POST.get('zipcode')
         city = request.POST.get('city')
         phone = request.POST.get('phone')
+        email = request.POST.get('email')
         obs = request.POST.get('obs')
-        s = Supplier(
-            name=name,
-            nif=nif,
-            email=email,
-            address=address,
-            zipcode=zipcode,
-            city=city,
-            phone=phone,
-            obs=obs
-        )
-        s.save()
-        print(
-            f"Inserted Supplier " + name + " " + nif + " " + address + " " + zipcode + " " + city + " " + phone + " " + obs)
+        with connections['admin'].cursor() as cursor:
+            # Call the stored procedure using the CALL statement
+            cursor.execute("CALL sp_suppliers_create(%s,%s,%s,%s,%s,%s,%s,%s)",
+                           [name, nif, address, zipcode, city, phone,email, obs])
+            # If the stored procedure returns results, you can fetch them
+            #result = cursor.fetchall()
+            #print(result)
+        print(f"Inserted supplier " + name + " " + nif + " " + address + " " + zipcode + " " + city + " " + phone + " " + email+ " " + obs)
+        # return render(request, 'your_template.html', {'result': result})
         return redirect('dashboard')
     return render(request, template_name='suppliersCreate.html')
 
