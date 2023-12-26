75d197ffa95a7498a370078d6ae9daf8292f6d75
hugosantos <hugo.santos@i9box.net>
2023-12-26 19:29:19 +0000

    encomenda

--- ByteBridges/ByteBridges/views.py
+++ ByteBridges/ByteBridges/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
-from .models import Supplier, Warehouse, Client,Family, Article, ArticleType, Equipment,ComponentListFamily
+from .models import Supplier, Warehouse, Client, Family, Article, ArticleType, Equipment, ComponentListFamily
 
 from django.db import connections
 from django.http import JsonResponse
 import json
+
+
 def Login(request):
     return render(request, "Login.html")
 
@@ -14,6 +16,7 @@ def loginForm(request):
 
 def IndexPage(request):
     return render(request, template_name='dashboard.html')
+
 
 # Clients
 def clientList(request):
@@ -45,8 +48,8 @@ def clientCreate(request):
         with connections['admin'].cursor() as cursor:
             # Call the stored procedure using the CALL statement
             cursor.execute("CALL sp_clients_create(%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-                           [email,individual,zipcode,address,nif,name,obs,city,eletronicinvoice])
-            
+                           [email, individual, zipcode, address, nif, name, obs, city, eletronicinvoice])
+
         return redirect('dashboard')
 
     # return the form
@@ -88,7 +91,6 @@ def orderClientCreate(request):
     return render(request, template_name='orderClientCreate.html')
 
 
-
 # Suppliers
 def supplierList(request):
     with connections['admin'].cursor() as cursor:
@@ -98,7 +100,7 @@ def supplierList(request):
         result = cursor.fetchall()
         print(result)
         suppliers = [Supplier(*row) for row in result]
-        return render(request,'supplierList.html',{'suppliers': suppliers})
+        return render(request, 'supplierList.html', {'suppliers': suppliers})
 
 
 def supplierCreate(request):
@@ -114,11 +116,12 @@ def supplierCreate(request):
         with connections['admin'].cursor() as cursor:
             # Call the stored procedure using the CALL statement
             cursor.execute("CALL sp_suppliers_create(%s,%s,%s,%s,%s,%s,%s,%s)",
-                           [name, nif, address, zipcode, city, phone,email, obs])
+                           [name, nif, address, zipcode, city, phone, email, obs])
             # If the stored procedure returns results, you can fetch them
-            #result = cursor.fetchall()
-            #print(result)
-        print(f"Inserted supplier " + name + " " + nif + " " + address + " " + zipcode + " " + city + " " + phone + " " + email+ " " + obs)
+            # result = cursor.fetchall()
+            # print(result)
+        print(
+            f"Inserted supplier " + name + " " + nif + " " + address + " " + zipcode + " " + city + " " + phone + " " + email + " " + obs)
         # return render(request, 'your_template.html', {'result': result})
         return redirect('dashboard')
     return render(request, template_name='supplierCreate.html')
@@ -132,7 +135,7 @@ def orderSupplierList(request):
         result = cursor.fetchall()
         print(result)
         suppliers = [Supplier(*row) for row in result]
-        return render(request,'orderSupplierList.html',{'suppliers': suppliers})
+        return render(request, 'orderSupplierList.html', {'suppliers': suppliers})
 
 
 def orderSupplierCreate(request):
@@ -140,7 +143,7 @@ def orderSupplierCreate(request):
         cursor.execute("select * from view_suppliers_list")
         result = cursor.fetchall()
         docNumber = [Supplier(*row) for row in result]
-        
+
         cursor.execute("select * from view_suppliers_list")
         result = cursor.fetchall()
         suppliers = [Supplier(*row) for row in result]
@@ -153,17 +156,26 @@ def orderSupplierCreate(request):
         result = cursor.fetchall()
         families = [Family(*row) for row in result]
 
-        context = {'suppliers': suppliers, 'warehouses': warehouses, 'families': families, 'docNumber':docNumber}
+        context = {'suppliers': suppliers, 'warehouses': warehouses, 'families': families, 'docNumber': docNumber}
 
     if request.method == 'POST':
         data = json.loads(request.POST.get('data'))
+
         header = json.loads(request.POST.get('header'))
-        
-        #cursor.execute("select id FROM fn_ordersupplier_create(%s,%s,%s,)",)
-    
+        print(header)
+        #create a new supplier enc header
+        cursor.execute("select * from view_families_list")
+        result = cursor.fetchall()
+
+        # cursor.execute("select id FROM fn_ordersupplier_create(%s,%s,%s,)",)
+
         for item in data:
-            cursor.execute("CALL sp_buy_create(%s,%s,%s)",
-                           [20,data.component,data.quantity])
+            with connections['admin'].cursor() as cursor:
+                print(item)
+                cursor.execute("CALL sp_buy_create(%s,%s,%s)",
+                               [17,
+                                item['component'],
+                                item['quantity']])
 
         return JsonResponse({'status': 'success'})
     return render(request, template_name='orderSupplierCreate.html', context=context)
@@ -188,7 +200,8 @@ def get_articles(request):
         print(data)
         return JsonResponse(data)
 
-#Family  sp_families_create
+
+# Family  sp_families_create
 def familyCreate(request):
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -196,31 +209,29 @@ def familyCreate(request):
         with connections['admin'].cursor() as cursor:
             # Call the stored procedure using the CALL statement
             cursor.execute("CALL sp_families_create(%s,%s)",
-                           [name,desc])
-  
+                           [name, desc])
+
         print(f"Inserted Family " + name + " " + desc)
         return redirect('dashboard')
     return render(request, template_name='familyCreate.html')
 
-#Equipment
+
+# Equipment
 def equipmentCreate(request):
-    
     with connections['admin'].cursor() as cursor:
         cursor.execute("select * from view_warehouses_list")
         result = cursor.fetchall()
-       
+
         warehouse = [Warehouse(*row) for row in result]
         print(warehouse)
-        
+
         cursor.execute("select * from view_families_list")
         result = cursor.fetchall()
-       
+
         families = [Family(*row) for row in result]
-        context={'warehouse':warehouse,'families':families}
+        context = {'warehouse': warehouse, 'families': families}
 
-
-
-    return render(request,'equipmentCreate.html',context=context)
+    return render(request, 'equipmentCreate.html', context=context)
 
 
 def equipmentList(request):
@@ -230,13 +241,12 @@ def equipmentList(request):
         # If the stored procedure returns results, you can fetch them
         result = cursor.fetchall()
         equipments = [Equipment(*row) for row in result]
-        context={'equipments':equipments}
-        return render(request,'equipmentList.html',context=context)
+        context = {'equipments': equipments}
+        return render(request, 'equipmentList.html', context=context)
 
 
 def componentCreate(request):
     with connections['admin'].cursor() as cursor:
-
         cursor.execute("select * from view_families_list")
         result = cursor.fetchall()
 
@@ -256,10 +266,11 @@ def componentCreate(request):
         with connections['admin'].cursor() as cursor:
             # Call the stored procedure using the CALL statement
             cursor.execute("CALL sp_articletypes_create(%s, %s, %s, %s, %s, %s, %s, %s)",
-                           [idfamily,idcategory, name, description, image, profit_margin, barcode, reference])
+                           [idfamily, idcategory, name, description, image, profit_margin, barcode, reference])
             return redirect('dashboard')
 
     return render(request, 'componentCreate.html', context=context)
+
 
 def componentList(request):
     with connections['admin'].cursor() as cursor:
@@ -268,4 +279,4 @@ def componentList(request):
         # If the stored procedure returns results, you can fetch them
         result = cursor.fetchall()
 
-        return render(request,'componentList.html',{'result':result})
+        return render(request, 'componentList.html', {'result': result})
